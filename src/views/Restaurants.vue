c1ee2d1fa76366c4050beb85020c208b4812a599
Ansticefish <ansticefish0106@gmail.com>
2022-05-08 22:26:19 +0800

    Upgrade style and provide better UX

--- src/views/Restaurants.vue
+++ src/views/Restaurants.vue
@@ -2,19 +2,22 @@
   <div class="container py-5">
     <NavTabs />
     <!-- NavPills -->
-    <RestaurantsNavPills :categories="categories" /> 
+    <RestaurantsNavPills :categories="categories" />
 
-    <!-- RestaurantCards -->
-    <div class="container">
-      <div class="row">
-        <RestaurantsCard v-for="restaurant in restaurants" :key="restaurant.id" :initial-restaurant="restaurant" />
+    <Spinner v-if="isLoading"/> 
+    <template v-else>
+      <!-- RestaurantCards -->
+      <div class="container">
+        <div class="row">
+          <RestaurantsCard v-for="restaurant in restaurants" :key="restaurant.id" :initial-restaurant="restaurant" />
+        </div>
       </div>
-    </div>
-    
-    <!-- Pagination -->
-    <RestaurantsPagination v-if="totalPage.length > 1" :category-id="categoryId" :current-page="currentPage" :total-page="totalPage" :previous-page="previousPage" :next-page="nextPage" />
-
+      
+      <!-- Pagination -->
+      <RestaurantsPagination v-if="totalPage.length > 1" :category-id="categoryId" :current-page="currentPage" :total-page="totalPage" :previous-page="previousPage" :next-page="nextPage" />
+    </template>
   </div>
+    
 </template>
 
 <script>
@@ -24,6 +27,7 @@ import RestaurantsNavPills from '../components/RestaurantsNavPills.vue'
 import RestaurantsPagination from '../components/RestaurantsPagination.vue'
 import restaurantsAPI from '../apis/restaurants'
 import { Toast } from '../utils/helpers'
+import Spinner from '../components/Spinner.vue'
 
 
 
@@ -34,6 +38,7 @@ export default {
     RestaurantsCard,
     RestaurantsNavPills,
     RestaurantsPagination,
+    Spinner,
   },
   data () {
     return {
@@ -43,12 +48,14 @@ export default {
       currentPage: 1,
       totalPage: [],
       previousPage: -1,
-      nextPage: -1
+      nextPage: -1,
+      isLoading: true
     }
   },
   methods: {
     async fetchData ({ queryPage, queryCategoryId }) {
       try {
+        this.isLoading = true
         const response = await restaurantsAPI.getRestaurants({ page: queryPage, categoryId: queryCategoryId })
 
         const {restaurants, categories, categoryId, page, totalPage, prev, next} = response.data
@@ -59,7 +66,9 @@ export default {
         this.totalPage = totalPage
         this.previousPage = prev
         this.nextPage = next
+        this.isLoading = false
       } catch (error) {
+        this.isLoading = false
           console.log('error', error)
           Toast.fire({
               icon: 'error',
